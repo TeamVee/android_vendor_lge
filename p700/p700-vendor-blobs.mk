69b920f80c3cdb0224325662375eabff2caea01c
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-04-29 17:25:08 -0300

    vendor: m4/v1/vee3/u0: Drop a lot of ril libs
    * Soon i will put new libs for this work
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- p700/p700-vendor-blobs.mk
+++ p700/p700-vendor-blobs.mk
@@ -21,13 +21,10 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p700/proprietary/lib/libchromatix_hi542_default_video.so:system/lib/libchromatix_hi542_default_video.so \
     vendor/lge/p700/proprietary/lib/libchromatix_hi542_preview.so:system/lib/libchromatix_hi542_preview.so \
     vendor/lge/p700/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
-    vendor/lge/p700/proprietary/bin/netmgrd:system/bin/netmgrd \
-    vendor/lge/p700/proprietary/bin/qmuxd:system/bin/qmuxd \
     vendor/lge/p700/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/lge/p700/proprietary/lib/libauth.so:system/lib/libauth.so \
     vendor/lge/p700/proprietary/lib/libcm.so:system/lib/libcm.so \
     vendor/lge/p700/proprietary/lib/libdiag.so:system/lib/libdiag.so \
-    vendor/lge/p700/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
     vendor/lge/p700/proprietary/lib/libdsm.so:system/lib/libdsm.so \
     vendor/lge/p700/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
     vendor/lge/p700/proprietary/lib/libidl.so:system/lib/libidl.so \
@@ -37,21 +34,5 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p700/proprietary/lib/liblgsecclk.so:system/lib/liblgsecclk.so \
     vendor/lge/p700/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
     vendor/lge/p700/proprietary/lib/libnv.so:system/lib/libnv.so \
-    vendor/lge/p700/proprietary/lib/liboem_rapi.so:system/lib/liboem_rapi.so \
     vendor/lge/p700/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
     vendor/lge/p700/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
-    vendor/lge/p700/proprietary/lib/libqcci_legacy.so:system/lib/libqcci_legacy.so \
-    vendor/lge/p700/proprietary/lib/libqdi.so:system/lib/libqdi.so \
-    vendor/lge/p700/proprietary/lib/libqdp.so:system/lib/libqdp.so \
-    vendor/lge/p700/proprietary/lib/libqmi_client_qmux.so:system/lib/libqmi_client_qmux.so \
-    vendor/lge/p700/proprietary/lib/libqmi_csvt_srvc.so:system/lib/libqmi_csvt_srvc.so \
-    vendor/lge/p700/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
-    vendor/lge/p700/proprietary/lib/libqmi.so:system/lib/libqmi.so \
-    vendor/lge/p700/proprietary/lib/libqueue.so:system/lib/libqueue.so \
-    vendor/lge/p700/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
-    vendor/lge/p700/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
-    vendor/lge/p700/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
-    vendor/lge/p700/proprietary/lib/libwcnftm.so:system/lib/libwcnftm.so \
-    vendor/lge/p700/proprietary/lib/libwfcu.so:system/lib/libwfcu.so \
-    vendor/lge/p700/proprietary/lib/libwms.so:system/lib/libwms.so \
-    vendor/lge/p700/proprietary/lib/libwmsts.so:system/lib/libwmsts.so
